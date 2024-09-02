663f0befa5868c18450862d73c2ff99027abb99d
Laxmiveer <laxmimore378@gmail.com>
2024-09-02 21:03:42 +0530

    Add files via upload

---
+++ Java/Reverse_num.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Reverse_num {
+    public static void main(String[] args) {
+        // for(int  =10;   >=1; i- -){
+        // System.out.println(i);
+        int i = 10;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
